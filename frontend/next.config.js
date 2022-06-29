04bed6c106e4b42af9b1315f3eda40305da0591a
rogerlam <roger.l.jiahao2@gmail.com>
2022-06-29 18:28:15 -0400

    Restart frontend with TypeScript

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: "default",
-    domains: ["localhost"],
-  },
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
